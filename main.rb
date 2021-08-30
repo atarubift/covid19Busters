8b55081ab135cfdd78e7da182060eb0fe0fc8018
okudas195014 <s195014@matsu.shimane-u.ac.jp>
2021-08-30 16:12:34 +0900

    加点になる的を表示

--- main.rb
+++ main.rb
@@ -1,8 +1,20 @@
 require 'dxruby'
+require_relative 'target.rb'
 
 Window.width = 800
 Window.height = 600
 Window.bgcolor = [0, 0, 0]
 
+targets = []
+targets << Target.new(200,100)
+targets << Target.new(500,100)
+targets << Target.new(200,300)
+targets << Target.new(500,300)
+
 Window.loop do
+
+    targets.each do |target|
+        target.update
+        target.draw
+    end
 end
---
+++ target.rb
@@ -0,0 +1,13 @@
+class Target < Sprite
+    
+    def initialize(x, y)
+        self.x = x
+        self.y = y
+        self.image = Image.load("images/virus.png")
+    end
+
+    def update
+
+    end
+
+end
