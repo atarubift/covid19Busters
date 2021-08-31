9661bb43739d54e7917866a244487c7f8e221637
ann2000 <roseanncherian@gmail.com>
2021-08-31 07:47:17 +0530

    targeting implemented

--- cursor.rb
+++ cursor.rb
@@ -7,5 +7,12 @@ class Cursor
         y = Input.mouse_pos_y 
 
         Window.draw(x, y, @image) 
+        
+        if Input.mouse_down?(M_LBUTTON) then   
+            Window.draw_font(x, y, "left", Font.new(32))
+        end
+        if Input.mouse_down?(M_RBUTTON) then
+            Window.draw_font(x, y, "right", Font.new(32))
+        end
     end
 end
