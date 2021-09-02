a38d1f09be4f880c42131bc0fbe9bf2bce23e18f
atarubift <atarubift@gmail.com>
2021-09-02 14:47:24 +0900

    Add shoot sound

--- cursor.rb
+++ cursor.rb
@@ -1,7 +1,13 @@
 class Cursor < Sprite
     def initialize
         self.image = Image.load("images/player.png")
+        f = 4000
+        @sound = SoundEffect.new(20, WAVE_TRI) do
+          f = f - 120
+          [f, 15]
+        end
     end
+
     def move       
         self.x = Input.mouse_pos_x
         self.y = Input.mouse_pos_y 
@@ -9,7 +15,8 @@ class Cursor < Sprite
         Window.draw(self.x, self.y, self.image) 
         
         if Input.mouse_down?(M_LBUTTON) then   
-            Window.draw_font(self.x, self.y, "left", Font.new(32))
+            @sound.play
+            @sound.save("images/marupakuri.wav")
         end
         if Input.mouse_down?(M_RBUTTON) then
             Window.draw_font(self.x, self.y, "right", Font.new(32))
