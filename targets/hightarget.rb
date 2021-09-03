9fbad09f13bd81d9f72e2e7ebe00efcdef2dacd1
okudas195014 <s195014@matsu.shimane-u.ac.jp>
2021-09-03 11:10:13 +0900

    fix double point score

--- targets/hightarget.rb
+++ targets/hightarget.rb
@@ -16,10 +16,14 @@ class HighTarget < Target
         self.class.collection.delete(self) if @time > 50
         
         if self.vanished?
-          if Time.now - @hitime < 0.5
-            Window.draw_font(375, 10, "#{sprintf("%+d", @score*2)}", @font, color: C_BLACK)
-          end
-      end
+            if Time.now - @hitime < 0.5
+              if $double_flag == 0
+                Window.draw_font(375, 10, "#{sprintf("%+d", @score)}", @font, color: C_BLACK)
+              else
+                Window.draw_font(375, 10, "#{sprintf("%+d", @score*2)}", @font, color: C_BLACK)
+              end
+            end
+        end
     end
 end
 
